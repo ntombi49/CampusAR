687319b4438187b43a54d9bbd0a378e3a7621e68
Nhlanhla Rikhotso <nhlanhlarikhotso606@gmail.com>
2026-07-11 20:16:56 +0200

    Add React Navigation skeleton with Login/Home/ClassSearch stack

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Yeahhhh buddy</Text>
+      <Text>can we start already</Text>
       <StatusBar style="auto" />
     </View>
   
